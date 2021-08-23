820cba7de233b238b6bfffbbc8dc1edcfc95efb8
Dino Ferizovic <74234240+dife55@users.noreply.github.com>
2021-08-23 23:26:53 +0200

    Added base functions

--- src/main.js
+++ src/main.js
@@ -1,6 +1,34 @@
 import { createApp } from 'vue'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+
+import firebase from 'firebase'
+
+
+
+
+// Required for side-effects
+require("firebase/firestore");
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAdsGSig2iThXL8tgax23QEC5IzGvw7Zos",
+    authDomain: "ciao-bene-chat.firebaseapp.com",
+    projectId: "ciao-bene-chat",
+    storageBucket: "ciao-bene-chat.appspot.com",
+    messagingSenderId: "685530573170",
+    appId: "1:685530573170:web:b494109b23fd5d01a9d5eb",
+    measurementId: "G-Q5CC08W7FE"
+  };
+
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var db = firebase.firestore();
+window.db = db;
 
 createApp(App).use(store).use(router).mount('#app')
--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,13 +1,56 @@
 <template>
-    <h1>This is the homepage</h1>
+
+
+  <div class="container-fluid">
+    <input
+      @keyup.enter="saveMessage"
+      v-model="message"
+      type="text"
+      class="form-control"
+      placeholder="Type a message"
+    />
+    <button class="btn btn-primary" type="button">SEND</button>
+  </div>
 </template>
 
 <script>
 export default {
-    components: {
+  name: "Chat",
 
-    }
-}
+  data() {
+    return {
+      message: null,
+      messages: [],
+    };
+  },
+
+  methods: {
+    saveMessage() {
+      db.collection("chat").add({
+        message: this.message,
+      });
+
+      this.message = null;
+    },
+
+    fetchMessages() {
+      db.collection("chat")
+        .get()
+        .then((querySnapshot) => {
+          let allMessages = [];
+          querySnapshot.forEach((doc) => {
+            allMessages.push(doc.data());
+          });
+
+          this.messages = allMessages;
+        });
+    },
+  },
+
+  created() {
+    this.fetchMessages();
+  },
+};
 </script>
 
 <style scoped>
