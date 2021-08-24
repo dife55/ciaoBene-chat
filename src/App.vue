b3f12ff071f8e3065b21793a491bd55ff0aee3a2
Dino Ferizovic <74234240+dife55@users.noreply.github.com>
2021-08-25 00:37:15 +0200

    New Commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,66 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <nav class="navbar navbar-dark bg-dark navbar-fixed-top">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="/">C I A O B E N E</a>
+        </div>
+        <div id="navbar">
+          <ul class="nav navbar-nav navbar-right">
+            <li>
+                <router-link to="/about">About</router-link>
+                  <router-link to="/signup"> Signup</router-link>
+              <router-link to="/login"> Login</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   </div>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Montserrat", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background-color: rgb(236, 236, 236);
 }
 
-#nav {
+nav{
+    -webkit-box-shadow: 0 8px 6px -6px rgb(124, 124, 124);
+  -moz-box-shadow: 0 8px 6px -6px rgb(153, 153, 153);
+  box-shadow: 0 8px 6px -6px rgb(153, 153, 153);
+  position: sticky;
+}
+
+.nav {
   padding: 30px;
 }
 
-#nav a {
+.navbar-brand {
   font-weight: bold;
-  color: #2c3e50;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+
+
+#nav a {
+  color: #ffffff;
+  text-decoration: none;
+  margin: 10px;
 }
+
+#nav a:hover {
+  color: #FE4C6F;
+}
+
+
+
+#nav a.router-link-exact-active {
+  color: #FE4C6F;
+}
+
 </style>
---
+++ src/views/Signup.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container">
+<form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" v-model="formData.email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" v-model="formData.password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+
+  <button class="btn btn-success" @click="signUp">SignUp</button>
+</form>
+  </div>
+</template>
+
+<script>
+import firebase from "firebase";
+export default {
+  name: "Signup",
+  data() {
+    return {
+      formData: {
+        email: "",
+        password: "",
+      },
+    };
+  },
+
+  methods: {
+    signUp() {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.formData.email, this.formData.password)
+        .then((user) => {
+
+          this.$router.replace('/login')
+
+          // Signed in
+          // var user = userCredential.user;
+          // ...
+        })
+        .catch((error) => {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // ..
+        });
+    },
+  },
+};
+</script>
+
+<style>
+</style>
