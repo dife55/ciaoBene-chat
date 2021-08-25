7f5fdc44c166c62a1a0ec60a0c06b788ff4e7df4
Dino Ferizovic <74234240+dife55@users.noreply.github.com>
2021-08-25 21:37:24 +0200

    Feature: added features

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,51 +1,94 @@
 <template>
   <div class="container">
-    <h3>Login</h3>
-          <div class="row">
-        <h2>Signin</h2>
 
-        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-md-offset-3">
-
-            <input type="email" v-model="formData.email" class="form-control" placeholder="email">
-            <br>
-            <input type="password" v-model="formData.password" class="form-control" placeholder="password">
-            <br>
-            <button class="btn btn-success" @click="signIn">Signin</button>
-        </div>
-
-    </div>
-
-
-    <button @click="login">Google Login</button>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+        <input
+          type="email"
+          v-model="formData.email"
+          class="form-control"
+          placeholder="email"
+        />
   </div>
+
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+        <input
+          type="password"
+          v-model="formData.password"
+          class="form-control"
+          placeholder="password"
+        />
+
+    <div class="password-hint-error" id="password-hint">Password must contain at least 6 characters</div>
+  </div>
+  <div class="d-grid gap-2">
+
+        <button class="btn btn-success" @click="signIn">Signin</button>
+          <button class="btn btn-success" @click="login">Login with Google</button>
+  </div>
+  </div>
+
 </template>
 
 <script>
 import firebase from "firebase";
-    export default {
-        name: 'SignIn',
-        data () {
-            return {
-                formData:{
-                    email:'',
-                    password:''
-                }
-            }
-        },
-        methods: {
-            signIn(){
-                firebase.auth().signInWithEmailAndPassword(this.formData.email,this.formData.password)
-                    .then((user)=>{
-                      this.$router.replace('/')
-                    })
-                    .catch((e)=>{
-                        alert(e.message)
-                    })
-            }
-        },
-        created(){
-        }
-    }
+export default {
+  name: "SignIn",
+  data() {
+    return {
+      formData: {
+        email: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    signIn() {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.formData.email, this.formData.password)
+        .then((user) => {
+          this.$router.replace("/");
+        })
+        .catch((e) => {
+          alert(e.message);
+        });
+    },
+
+    login() {
+      var provider = new firebase.auth.GoogleAuthProvider();
+      provider.addScope("https://www.googleapis.com/auth/contacts.readonly");
+
+      firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then((result) => {
+          /** @type {firebase.auth.OAuthCredential} */
+          var credential = result.credential;
+
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          var token = credential.accessToken;
+          // The signed-in user info.
+          var user = result.user;
+
+          this.$router.push('/')
+          // ...
+        })
+        .catch((error) => {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+          // ...
+        });
+    },
+  },
+  created() {},
+};
 </script>
 
 <style>
