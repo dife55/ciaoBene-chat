24d23c4927b1d4a1f8dc1d72045121b28138621e
Dino Ferizovic <74234240+dife55@users.noreply.github.com>
2021-08-25 21:37:37 +0200

    Fix: cosmetic changes

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="container">
 <form>
+
   <div class="form-group">
     <label for="exampleInputEmail1">Email address</label>
     <input type="email" v-model="formData.email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
   </div>
+
   <div class="form-group">
     <label for="exampleInputPassword1">Password</label>
-    <input type="password" v-model="formData.password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    <input type="password" minlength="6" v-model="formData.password" class="form-control" id="exampleInputPassword1" aria-describedby="password-hint" placeholder="Password">
+    <div class="password-hint-error" id="password-hint">Password must contain at least 6 characters</div>
   </div>
-
+  <div class="d-grid gap-2">
   <button class="btn btn-success" @click="signUp">SignUp</button>
+  <button class="btn btn-success" @click="login">I have a google account</button>
+  </div>
 </form>
   </div>
 </template>
@@ -34,12 +39,11 @@ export default {
         .auth()
         .createUserWithEmailAndPassword(this.formData.email, this.formData.password)
         .then((user) => {
-
-          this.$router.replace('/login')
-
+          this.$router.push('Login')
           // Signed in
           // var user = userCredential.user;
           // ...
+          username = this.formData.username;
         })
         .catch((error) => {
           var errorCode = error.code;
