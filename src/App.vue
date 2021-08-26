0a8b57793e81cc4162d0f7d8813d78da07fd76e6
Dino Ferizovic <74234240+dife55@users.noreply.github.com>
2021-08-26 14:21:29 +0200

    Fix: fixed the nav bar so its fixed on top

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,88 @@
 <template>
-  <div id="nav">
+  <div class="fixed-top" id="nav">
     <head>
-      
-    <nav class="navbar navbar-dark bg-dark navbar-fixed-hrefp">
-      <div class="container">
-        <div class="navbar-header">
-          <a class="navbar-brand" href="/">C I A O B E N E</a>
-        </div>
-        <div id="navbar">
-          <ul class="nav navbar-nav navbar-right">
-            <li>
+      <nav class="navbar navbar-dark bg-dark navbar-fixed-top">
+        <div class="container">
+          <div class="navbar-header">
+            <a class="navbar-brand" href="/">C I A O B E N E</a>
+          </div>
+          <div id="navbar">
+            <ul class="nav navbar-nav navbar-right">
+              <li>
                 <a href="/about">About</a>
-                  <a href="/sign-up"> Signup</a>
-              <a href="/login"> Login</a>
-            </li>
-          </ul>
+                <a href="/sign-up"> Signup</a>
+                <a href="/login"> Login</a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
     </head>
   </div>
   <router-view />
 </template>
+
+<script>
+import firebase from "firebase";
+export default {
+  name: "SignIn",
+  data() {
+    return {
+      formData: {
+        email: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    signIn() {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.formData.email, this.formData.password)
+        .then((user) => {
+          this.$router.replace("/");
+        })
+        .catch((e) => {
+          alert(e.message);
+        });
+    },
+
+    login() {
+      var provider = new firebase.auth.GoogleAuthProvider();
+      provider.addScope("https://www.googleapis.com/auth/contacts.readonly");
+
+      firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then((result) => {
+          /** @type {firebase.auth.OAuthCredential} */
+          var credential = result.credential;
+
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          var token = credential.accessToken;
+          // The signed-in user info.
+          var user = result.user;
+
+          this.$router.push('/')
+          // ...
+        })
+        .catch((error) => {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+          // ...
+        });
+    },
+  },
+  created() {},
+};
+
+</script>
+
 
 <style>
 #app {
@@ -30,13 +91,17 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   background-color: rgb(236, 236, 236);
+  margin-top: 20vh;
 }
 
-nav{
-    -webkit-box-shadow: 0 8px 6px -6px rgb(124, 124, 124);
+html {
+  background: rgb(236, 236, 236);
+}
+
+nav {
+  -webkit-box-shadow: 0 8px 6px -6px rgb(124, 124, 124);
   -moz-box-shadow: 0 8px 6px -6px rgb(153, 153, 153);
   box-shadow: 0 8px 6px -6px rgb(153, 153, 153);
-  position: sticky;
 }
 
 .nav {
@@ -47,8 +112,6 @@ nav{
   font-weight: bold;
 }
 
-
-
 #nav a {
   color: #ffffff;
   text-decoration: none;
@@ -56,13 +119,26 @@ nav{
 }
 
 #nav a:hover {
-  color: #FE4C6F;
+  color: #fe4c6f;
 }
-
-
 
 #nav a.a-exact-active {
-  color: #FE4C6F;
+  color: #fe4c6f;
 }
 
+button {
+  background: #000000;
+  box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
+  transition: all 0.3s ease 0s;
+  font-weight: bold;
+  font-size: 0.7rem;
+  width: 100%;
+}
+
+button:hover {
+  border: 0px;
+  background-color: #fe4c6f;
+  box-shadow: 0px 15px 20px rgba(78, 78, 78, 0.4);
+  color: #fff;
+}
 </style>
